8bc5006e4de0e32919bba1600b3fb61806b9e495
Tawhidul Arefin <tawhidularefin530@gmail.com>
2020-04-10 18:24:41 +0600

    Update settings.py

--- coresite/settings.py
+++ coresite/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = '2*ky6&fzf&(1ys1g=hzoz8w!^v)z7ig4td$1@qdvz^(#eigb9o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coronavirus1314.herokuapp.com']
 
 
 # Application definition
