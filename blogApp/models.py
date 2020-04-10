b423805740528f435cfcbb3b5aff5079b49dce97
Tawhidul Arefin <tawhidularefin530@gmail.com>
2020-04-11 02:18:06 +0600

    Update models.py

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,16 +1,41 @@
 from django.db import models
-
+from django.utils import timezone
 from userApp.models import UserProfile
 
+class Category(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated at")
+    title = models.CharField(max_length=255, verbose_name="Title")
 
-class Blog(models.Model):
-    created_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     title = models.CharField("Title", max_length=300)
     featured_img = models.ImageField(null=True, blank=True)
     description = models.TextField()
-    blog_tag = models.ManyToManyField("Tag")
+    post_tag = models.ManyToManyField("Tag")
+    category = models.ForeignKey(Category, verbose_name="Category")
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False, verbose_name="Is published?")
+    published_at = models.DateTimeField(null=True, blank=True, editable=False, verbose_name="Published at")
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        ordering = ['-created_at']
+
+    def publish(self):
+        self.is_published = True
+        self.published_at = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.title
@@ -23,3 +48,9 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.tag_name
+
+class Comment(models.Model):
+    created_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
