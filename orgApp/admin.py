6c9c21ef9d879dec919ec98c215f38f09d431b7c
jubel <jubel8180@gmail.com>
2020-04-27 04:57:51 +0600

    model add and update

--- orgApp/admin.py
+++ orgApp/admin.py
@@ -3,6 +3,10 @@ from orgApp import models
 
 # Register your models here.
 
+admin.site.register(models.Country)
+admin.site.register(models.City)
+admin.site.register(models.District)
+admin.site.register(models.Thana)
 admin.site.register(models.Organisation)
 admin.site.register(models.Category)
 admin.site.register(models.orgDetail)
