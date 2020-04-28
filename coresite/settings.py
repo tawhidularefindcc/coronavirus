ee4261fef3d29edaa80b8fab3be58c0390ae9fa8
jubel <jubel8180@gmail.com>
2020-04-28 20:06:12 +0600

    organization reg form add and model problem resolve

--- coresite/settings.py
+++ coresite/settings.py
@@ -49,8 +49,11 @@ INSTALLED_APPS = [
     'crispy_forms',
     'userApp.apps.UserappConfig',  # User Application for User Handle
     'orgApp',
+    'mediumeditor',
+  
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 MIDDLEWARE = [
@@ -124,7 +127,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'bn'
 
 TIME_ZONE = 'UTC'
 
--- orgApp/views.py
+++ orgApp/views.py
@@ -3,10 +3,14 @@ from .models import *
 from django.http import HttpRequest, HttpResponsePermanentRedirect
 from django.shortcuts import reverse
 from orgApp import forms
+from userApp.models import UserProfile
+from .models import Organisation
+
 
 # Create your views here.
 def test(request):
     return render(request, 'index_main.html', {'nbar': 'home'})
+
 
 def home(request):
     context = {
@@ -16,27 +20,34 @@ def home(request):
 
 
 def self_org(request: HttpRequest):
-    logged_in_user = request.user
+    # logged_in_user = request.user
+    # user = UserProfile.objects.all()[:1].get()
     queryset = Organisation.objects.filter()
-    new = True if queryset.count() == 0 else None
+    # new = True if queryset.count() == 0 else None
+    new = True
     context = {'queryset': queryset, 'errorMsg': None, 'new': new}
 
     # check for request type
-
+    context['nbar'] = 'org_reg'
     if request.method == 'GET':
         if new:
             form = forms.OrganisationMainRegForm()
+            org_detail_form = forms.OrgDetailMainRegForm()
+
             context['orgForm'] = form
+            context['org_detail_form'] = org_detail_form
         return render(request, 'orgApp/selfOrg.html', context)
     elif request.method == 'POST':
         data = request.POST
+
         if new:
             form = forms.OrganisationMainRegForm(data)
-            if form.is_valid():
-                form.save(commit=False)
-                form.owner_id = logged_in_user
-                form.save()
+            orgForm = forms.OrgDetailMainRegForm(data, request.FILES)
+            if form.is_valid() and orgForm.is_valid():
+                new_org = form.save()
+                orgForm.save(commit=False)
+                orgForm.organisation = new_org
+                orgForm.save()
                 return HttpResponsePermanentRedirect(reverse("orgApplication:self_org"))
         context['errorMsg'] = 'Not new user'
         return render(request, 'orgApp/selfOrg.html', context)
-
